dbb1005b04bd39949fbd6b9fde192a501df0e147
nikitabogdanov <nmbogdan@alumni.stanford.edu>
2025-07-18 02:24:04 -0400

    Fix typo

--- backend/app.py
+++ backend/app.py
@@ -156,7 +156,7 @@ search_blp = Blueprint(
 )
 
 dumps_blp = Blueprint("dumps", 'dumps', url_prefix="/api/dumps",
-    description="Access bulk data stored on Cloudare")
+    description="Access bulk data on Cloudflare")
 
 class SearchArgsSchema(Schema):
     year = fields.List(fields.Int(), load_default=[])
