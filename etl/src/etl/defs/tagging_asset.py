5cc8f3c0786681b458c8649f1f51fa93943936d9
nikitabogdanov <nmbogdan@alumni.stanford.edu>
2025-07-30 01:01:42 -0600

    Add core db logic

--- etl/src/etl/defs/tagging_asset.py
+++ etl/src/etl/defs/tagging_asset.py
@@ -3,42 +3,53 @@ from sqlalchemy import text
 from etl.domain.tagging import tag
 import dagster as dg
 from etl.defs.staging_asset import staging_asset
+from etl.utils.db_utils import upsert_tags
 
 
 @dg.asset(deps=[staging_asset])
 def tagging_asset(db: DBResource, tagging_model: TaggingModel):
-    """ """
-    pass
-    # batch_size: int = 15
-    # last_uuid: str = ""
-    # engine = db.get_engine()
+    """
+    Pulls staged pages and runs them through the tagging model.
+    Args:
+        db (DBResource): Database resource for connection.
+        tagging_model (TaggingModel): Model for page tagging.
+    Returns:
+        None
+    """
+    tagging_model = tagging_model.model()
+    
+    batch_size: int = 250
+    last_uuid: str = ""
+    engine = db.get_engine()
 
-    # with engine.begin() as conn:
-    #     while True:
-    #         # fetch batch of staged (not processed) agreements
-    #         result = conn.execute(
-    #             text(
-    #                 """
-    #                 SELECT agreement_uuid, url
-    #                 FROM pdx.agreements
-    #                 WHERE agreement_uuid > :last_uuid
-    #                 AND processed = 0
-    #                 ORDER BY agreement_uuid ASC
-    #                 LIMIT :batch_size
-    #             """
-    #             ),
-    #             {"last_uuid": last_uuid, "batch_size": batch_size},
-    #         )
-    #         rows = result.fetchall()
-    #         if not rows:
-    #             break
+    with engine.begin() as conn:
+        while True:
+            # fetch batch of staged (not tagged) pages
+            result = conn.execute(
+                text(
+                    """
+                    SELECT page_uuid, processed_page_content
+                    FROM pdx.pages
+                    WHERE page_uuid > :last_uuid
+                    AND processed = 0
+                    ORDER BY page_uuid ASC
+                    LIMIT :batch_size
+                """
+                ),
+                {"last_uuid": last_uuid, "batch_size": batch_size},
+            )
+            rows = result.mappings().fetchall()
 
-    #         agreements = [{"agreement_uuid": r[0], "url": r[1]} for r in rows]
-    #         staged_pages = pre_process(agreements, classifier_model)
-    #         try:
-    #             upsert_pages(staged_pages, conn)
-    #         except Exception as e:
-    #             print(f"Error upserting pages: {e}")
-    #             break
+            if not rows:
+                break
 
-    #         last_uuid = rows[-1][0]
+            # tag pages
+            tagged_pages = tag(rows, tagging_model)
+
+            try:
+                upsert_tags(tagged_pages, conn)
+            except Exception as e:
+                print(f"Error upserting tags: {e}")
+                raise RuntimeError(e)
+
+            last_uuid = rows[-1]['page_uuid']
