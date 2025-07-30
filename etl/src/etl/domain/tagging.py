594a419e7870aa91285e57696f346b7b065bc504
nikitabogdanov <nmbogdan@alumni.stanford.edu>
2025-07-30 01:01:55 -0600

    Add core logic

--- etl/src/etl/domain/tagging.py
+++ etl/src/etl/domain/tagging.py
@@ -4,8 +4,30 @@ from typing import List
 
 @dataclass
 class TagData:
-    tag: str
+    page_uuid: str
+    tagged_text: str
+    low_count: int
+    spans: list
+    chars: dict
 
 
-def tag(rows, classifier_model) -> List[TagData]:
-    pass
+def tag(rows, tagging_model) -> List[TagData]:
+
+    texts = [r["processed_page_content"] for r in rows]
+    page_uuids = [r["page_uuid"] for r in rows]
+
+    tagged_texts = tagging_model.label(texts)
+
+    staged_tagged = []
+    for page_uuid, tagged in zip(page_uuids, tagged_texts):
+        staged_tagged.append(
+            TagData(
+                page_uuid=page_uuid,
+                tagged_text=tagged["tagged"],
+                low_count=tagged["low_count"],
+                spans=tagged["spans"],
+                chars=tagged["chars"],
+            )
+        )
+    
+    return staged_tagged
