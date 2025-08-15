e95c07e1d918ce787b65d55a87ff51b25280d696
nikitabogdanov <nmbogdan@alumni.stanford.edu>
2025-08-15 10:48:34 -0400

    Initial commit

--- etl/src/etl/defs/__init__.py
+++ etl/src/etl/defs/__init__.py
@@ -1,1 +1,6 @@
+"""Dagster asset definitions for the ETL pipeline.
+
+This module contains the asset definitions that compose the ETL pipeline,
+including staging, pre-processing, tagging, and XML generation assets.
+"""
 
