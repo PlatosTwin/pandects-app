51fdc472c4389d761108fdda67fad4329294df61
nikitabogdanov <nmbogdan@alumni.stanford.edu>
2025-09-14 01:44:24 -0400

    Fix resource definition

--- etl/src/etl/definitions.py
+++ etl/src/etl/definitions.py
@@ -2,12 +2,12 @@
 from pathlib import Path
 from dagster import Definitions, load_from_defs_folder
 from etl.defs.jobs import etl_pipeline, cleanup_pipeline
-from etl.defs.resources import get_resources
+# from etl.defs.resources import get_resources
 
 defs = Definitions.merge(
     load_from_defs_folder(project_root=Path(__file__).parent.parent.parent),
     Definitions(
         jobs=[etl_pipeline, cleanup_pipeline],
-        resources=get_resources(),
+        # resources=get_resources(),
     ),
 )
