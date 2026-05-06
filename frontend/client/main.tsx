e8a75078c6da25e4cfcd36fc21d4c65e76225c6c
nikitabogdanov <nmbogdan@alumni.stanford.edu>
2026-05-06 15:57:16 -0400

    Restore head-loaded Google Analytics

--- frontend/client/main.tsx
+++ frontend/client/main.tsx
@@ -8,7 +8,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, useEffect } from "react";
 import {
   installGlobalErrorTracking,
-  scheduleAnalyticsScriptLoad,
   scheduleWhenBrowserIdle,
 } from "@/lib/analytics";
 import { AppLayout } from "@/components/AppLayout";
@@ -46,14 +45,12 @@ const DataLicense = lazy(() => import("./pages/DataLicense"));
 
 const App = () => {
   useEffect(() => {
-    const cancelScheduledAnalytics = scheduleAnalyticsScriptLoad();
     let cleanup = () => undefined;
     const cancelScheduledInstall = scheduleWhenBrowserIdle(() => {
       cleanup = installGlobalErrorTracking();
     });
 
     return () => {
-      cancelScheduledAnalytics();
       cancelScheduledInstall();
       cleanup();
     };
